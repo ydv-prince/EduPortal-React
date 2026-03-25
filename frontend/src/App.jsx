c891ff04f992093617ad1182f384e8eb07083aba
Rahul Kumar Yadav <ry642499@gmail.com>
2026-03-25 19:10:00 +0530

    integrate main app component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import AppRoutes from "./routes/AppRoutes";
+
+// import { BrowserRouter } from "react-router-dom";
+
+
+
+
+function App() {
+  return (
+    <>
+   <AppRoutes />
+
+
+
+    {/* <BrowserRouter>
+
+   </BrowserRouter>  */}
+   
+
+  </>
+)
+}
+
+export default App;
