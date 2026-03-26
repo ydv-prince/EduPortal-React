5cb0fcd95a31c939fb7ea23d4ec5fb9cf2ba95f5
Prince Kumar <pkumar052@rku.ac.in>
2026-03-26 10:15:00 +0530

    create user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
