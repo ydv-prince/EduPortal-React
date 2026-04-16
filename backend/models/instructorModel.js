eef5ed398aee6cc634c5da6ed2e628761716e22e
Raj226295 <rvardhan692@rku.ac.in>
2026-04-16 10:20:00 +0530

    create instructor model

---
+++ backend/models/instructorModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const instructorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Instructor', instructorSchema);
